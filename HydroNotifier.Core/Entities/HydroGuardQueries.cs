7b2c735b32a3fd1f2a7e8b3c17b707f281e327ca
David Ledvinka <SSP\ak204305>
2024-09-04 20:58:39 +0200

    data source updated

--- HydroNotifier.Core/Entities/HydroGuardQueries.cs
+++ HydroNotifier.Core/Entities/HydroGuardQueries.cs
@@ -2,9 +2,12 @@
 
 public class HydroGuardQueries
 {
-    public static readonly HydroQuery LomnaQuery = new HydroQuery("Lomná", "https://pocasi.idnes.cz/reky/jablunkov.id3547");
-    public static readonly HydroQuery OlseQuery = new HydroQuery("Olše", "https://pocasi.idnes.cz/reky/jablunkov.id3638");
+    public static readonly HydroQuery LomnaQuery = new HydroQuery("Lomná", "https://www.pod.cz/portal/SaP/cz/pc/Mereni.aspx?id=300021330&oid=2");
+    public static readonly HydroQuery OlseQuery = new HydroQuery("Olše", "https://www.pod.cz/portal/SaP/cz/pc/Mereni.aspx?id=300044843&oid=2");
 
     // alternative queries
     // https://pocasi.idnes.cz/?data=pocasi/prutoky/pocasi_prutoky_graf_3547.txt
+    // https://www.pod.cz/portal/SaP/cz/pc/Mereni.aspx?id=300021330&oid=2
+    //public static readonly HydroQuery LomnaQuery = new HydroQuery("Lomná", "https://pocasi.idnes.cz/reky/jablunkov.id3547");
+    //public static readonly HydroQuery OlseQuery = new HydroQuery("Olše", "https://pocasi.idnes.cz/reky/jablunkov.id3638");
 }
--- HydroNotifier.Core/Entities/WebScraper.cs
+++ HydroNotifier.Core/Entities/WebScraper.cs
@@ -3,6 +3,7 @@
 using System.Globalization;
 using System.Net.Http;
 using System.Text;
+using System.Xml.XPath;
 using HtmlAgilityPack;
 using Microsoft.Extensions.Logging;
 
@@ -27,10 +28,13 @@ public class WebScraper
         var doc = new HtmlDocument();
         doc.LoadHtml(html);
 
-        var flowValueElement = doc.DocumentNode.SelectSingleNode("//*[@id=\"reload_text\"]/div[1]/table/tbody/tr[2]/td[2]/b");
-        var dateValueElement = doc.DocumentNode.SelectSingleNode("//*[@id=\"reload_text\"]/div[1]/table/tbody/tr[3]/td[2]/b");
-        var dateValue = dateValueElement.InnerText;
-        var flowValue = flowValueElement.InnerText.Split(" ")[0];
+        var table = doc.DocumentNode.SelectSingleNode("//*[@id=\"ObsahCPH_DataMereniGV\"]");
+        var rows = table.SelectNodes("tr");
+        var cells = rows[1].SelectNodes("td");
+        var dateValueElement = cells[0];
+        var flowValueElement = cells[2];
+        var dateValue = dateValueElement.InnerText.Trim();
+        var flowValue = flowValueElement.InnerText.Trim().Split(" ")[0];
         var flowLitersPerSecond = double.Parse(flowValue, NumberStyles.Any, new CultureInfo("cs-CZ")) * 1000.0;
         _log.LogInformation($"Name = '{_query.Name}', Date = '{dateValue}', FlowInLitersPerSecond = '{flowLitersPerSecond}'");
 
